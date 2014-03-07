f31ca3de194c2926afa62db735849aff4f24a31d
ryoogata <ryo.ogata@gmail.com>
2014-03-08 00:01:04 +0900

    sysctl への依存関係を追加

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'All rights reserved'
 description      'Installs/Configures utility'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'sysctl'
+
 
 recipe "traincost::server",
   "Setup TrainCost Management System"
