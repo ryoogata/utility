543d34d60a019d4aa0fe54c93b4a954ac1b5153e
ryoogata <ryo.ogata@gmail.com>
2014-04-04 10:54:06 +0900

    recipe の記載方法を変更

--- metadata.rb
+++ metadata.rb
@@ -7,36 +7,13 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'sysctl'
 
-
-recipe "traincost::server",
-  "Setup TrainCost Management System"
-
-recipe "utility::bash_conf",
-  "Put bash_conf"
-
-recipe "utility::bind-utils",
-  "Install bind-utils"
-
-recipe "utility::dos2unix",
-  "Install dos2unix"
-
-recipe "utility::git",
-  "Install git"
-
-recipe "utility::git_rpm",
-  "Install git via RPM files"
-
-recipe "utility::jq",
-  "Install jq"
-
-recipe "utility::jsonpp",
-  "Install jsonapp"
-
-recipe "utility::lv",
-  "Install lv"
-
-recipe "utility::vim",
-  "Install vim"
-
-recipe "utility::yajl",
-  "Install yajl"
+recipe "utility::bash_conf", "Put bash_conf"
+recipe "utility::bind-utils", "Install bind-utils"
+recipe "utility::dos2unix", "Install dos2unix"
+recipe "utility::git", "Install git"
+recipe "utility::git_rpm", "Install git via RPM files"
+recipe "utility::jq", "Install jq"
+recipe "utility::jsonpp", "Install jsonapp"
+recipe "utility::lv", "Install lv"
+recipe "utility::vim", "Install vim"
+recipe "utility::yajl", "Install yajl"
