f2ff760ecf022da702eaaa91e2dc9e60f2599d87
ryoogata <ryo.ogata@gmail.com>
2014-04-25 14:24:38 +0900

    foodcritic FC008 指摘の修正

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'utility'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Ryo Ogata'
+maintainer_email 'ryo.ogata@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures utility'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
