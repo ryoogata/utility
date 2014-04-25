7ae2fbe072c66ab1b383bfa21ecf13269a847b31
ryoogata <ryo.ogata@gmail.com>
2014-04-25 14:21:12 +0900

    attribute による設定値の指定をできるように修正

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,11 @@ default["yajl"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
 default["tsunami"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
 
 default["berkshelf"]["version"] = "2.0.15"
+
+default["sysctl"]["net"]["core"]["somaxconn"] = 10240
+default["sysctl"]["net"]["core"]["netdev_max_backlog"] = 10240
+default["sysctl"]["net"]["ipv4"]["tcp_max_syn_backlog"] = 10240
+default["sysctl"]["net"]["ipv4"]["tcp_tw_recycle"] = 1
+default["sysctl"]["net"]["ipv4"]["tcp_tw_reuse"] = 1
+default["sysctl"]["net"]["ipv4"]["ip_local_port_range"] = '1024 65535'
+default["sysctl"]["net"]["ipv4"]["tcp_fin_timeout"] = 10
--- recipes/sysctl.rb
+++ recipes/sysctl.rb
@@ -1,31 +1,27 @@
 sysctl_param 'net.core.somaxconn' do
-  value 10240
+  value node['sysctl']['net']['core']['somaxconn']
 end
 
 sysctl_param 'net.core.netdev_max_backlog' do
-  value 10240
+  value node['sysctl']['net']['core']['netdev_max_backlog']
 end
 
 sysctl_param 'net.ipv4.tcp_max_syn_backlog' do
-  value 10240
-end
-
-sysctl_param 'net.ipv4.tcp_max_syn_backlog' do
-  value 10240
+  value node['sysctl']['net']['ipv4']['tcp_max_syn_backlog']
 end
 
 sysctl_param 'net.ipv4.tcp_tw_recycle' do
-  value 1
+  value node['sysctl']['net']['ipv4']['tcp_tw_recycle']
 end
 
 sysctl_param 'net.ipv4.tcp_tw_reuse' do
-  value 1
+  value node['sysctl']['net']['ipv4']['tcp_tw_reuse']
 end
 
 sysctl_param 'net.ipv4.ip_local_port_range' do
-  value '1024 65535'
+  value node['sysctl']['net']['ipv4']['ip_local_port_range']
 end
 
 sysctl_param 'net.ipv4.tcp_fin_timeout' do
-  value 10
+  value node['sysctl']['net']['ipv4']['tcp_fin_timeout']
 end
