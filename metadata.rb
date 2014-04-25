22ffdfaa67acd6dcc7e64443c6edb2cda6f38b92
ryoogata <ryo.ogata@gmail.com>
2014-04-25 14:26:39 +0900

    attribute の記述の追加

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'sysctl'
 
+
+# == Recipes
+#
+
 recipe "utility::bash_conf", "Put bash_conf"
 recipe "utility::bind-utils", "Install bind-utils"
 recipe "utility::dos2unix", "Install dos2unix"
@@ -17,3 +21,62 @@ recipe "utility::jsonpp", "Install jsonapp"
 recipe "utility::lv", "Install lv"
 recipe "utility::vim", "Install vim"
 recipe "utility::yajl", "Install yajl"
+
+# == Attributes
+# 
+
+attribute "utility",
+    :display_name => "utility",
+    :type => "hash"
+
+attribute "git/user/name",
+  :display_name => "",
+  :description => "",
+  :required => "optional",
+  :recipes => [
+    "utility::git"
+  ]
+
+attribute "git/user/email",
+  :display_name => "",
+  :description => "",
+  :required => "optional",
+  :recipes => [
+    "utility::git"
+  ]
+
+attribute "cvskit/_SOURCE_DOWNLOAD_DIR",
+  :display_name => "",
+  :description => "",
+  :default => "/tmp",
+  :required => "required",
+  :recipes => [
+    "utility::cvskit"
+  ]
+
+attribute "yajl/_SOURCE_DOWNLOAD_DIR",
+  :display_name => "",
+  :description => "",
+  :default => "/tmp",
+  :required => "required",
+  :recipes => [
+    "utility::yajl"
+  ]
+
+attribute "tsunami/_SOURCE_DOWNLOAD_DIR",
+  :display_name => "",
+  :description => "",
+  :default => "/tmp",
+  :required => "required",
+  :recipes => [
+    "utility::tsunami"
+  ]
+
+attribute "berkshelf/version",
+  :display_name => "",
+  :description => "",
+  :default => "2.0.15",
+  :required => "required",
+  :recipes => [
+    "utility::berkshelf"
+  ]
