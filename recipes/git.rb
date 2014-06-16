67d1b286f62c5de6ac2ddf9c5690f0727bbd6a9b
ryoogata <ryo.ogata@gmail.com>
2014-06-16 15:36:37 +0900

    CentOS と Amazon Linux で .gitconfig の設置場所を変える

--- recipes/git.rb
+++ recipes/git.rb
@@ -2,9 +2,20 @@ package "git" do
   action :install
 end
 
-template "/root/.gitconfig" do
-  source "gitconfig.erb"
-  owner "root"
-  group "root"
-  mode 0644
+case node['platform']
+when "centos"
+  template "/root/.gitconfig" do
+    source "gitconfig.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+when "amazon"
+  template "/home/ec2-user/.gitconfig" do
+    source "gitconfig.erb"
+    owner "ec2-user"
+    group "ec2-user"
+    mode 0644
+  end
 end
+
