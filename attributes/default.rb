32b428f0d621863768c0bde495ce4f155fed1911
ryoogata <ryo.ogata@gmail.com>
2014-07-28 12:15:51 +0900

    recipe:user 用 の attributes を追加

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default["git"]["user"]["email"] = ""
 default["cvskit"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
 default["yajl"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
 default["tsunami"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
-
+default["checkinstall"]["_SOURCE_DOWNLOAD_DIR"] = "/mnt"
 default["berkshelf"]["version"] = "2.0.15"
 
 default["sysctl"]["net"]["core"]["somaxconn"] = 10240
@@ -14,3 +14,5 @@ default["sysctl"]["net"]["ipv4"]["tcp_tw_recycle"] = 1
 default["sysctl"]["net"]["ipv4"]["tcp_tw_reuse"] = 1
 default["sysctl"]["net"]["ipv4"]["ip_local_port_range"] = '1024 65535'
 default["sysctl"]["net"]["ipv4"]["tcp_fin_timeout"] = 10
+
+default["user"]["data_bags"] = "users"
